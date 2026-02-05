17bc123d888b38213a52739747761720fda72f8e
Mykola Melnyk <mykolamelnyk338@gmail.com>
2026-02-05 17:27:56 +0200

    Change workflow file

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   // Services
-  API_BASE_URL: 'http://136.110.189.180',
-  OAUTH2_GOOGLE_AUTHORIZATION_URL: 'http://136.110.189.180/api/oauth2/authorization/google',
+  API_BASE_URL: 'http://136.110.189.180.nip.io',
+  OAUTH2_GOOGLE_AUTHORIZATION_URL: 'http://136.110.189.180.nip.io/api/oauth2/authorization/google',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
